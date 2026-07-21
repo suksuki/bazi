ec90610c119e1e7a4a0a08465963c994c16b52e3
suksuki <liusuksuki@gmail.com>
2026-07-21 23:17:54 +0900

    chore: retire V40 codebase

--- qiazhi/v40/v40/decision/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from v40.decision.domain_adapters import build_domain_adapter_signals
-from v40.decision.engine import build_decision_output
-
-__all__ = ["build_decision_output", "build_domain_adapter_signals"]
--- qiazhi/v40/v40/storage/__init__.py
+++
@@ -1,8 +0,0 @@
-from v40.storage.config import V40DatabaseConfig, resolve_v40_database_config
-from v40.storage.postgres import V40PostgresRepository
-
-__all__ = [
-    "V40DatabaseConfig",
-    "V40PostgresRepository",
-    "resolve_v40_database_config",
-]
